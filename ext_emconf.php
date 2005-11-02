cfdfaaaa8451c2ae9f2dbeca00ee7eec6cb2ba24
andreas <andreas@735d13b6-9817-0410-8766-e36946ffe9aa>
2005-11-02 09:44:10 +0000

    Raised version number to 1.0.0.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dkd_staticupload_scp/trunk@565 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,9 +2,9 @@
 
 ########################################################################
 # Extension Manager/Repository config file for ext: "dkd_staticupload_scp"
-# 
-# Auto generated 17-08-2005 12:14
-# 
+#
+# Auto generated 12-08-2005 12:37
+#
 # Manual updates:
 # Only the data in the array - anything else is removed by next write
 ########################################################################
@@ -31,8 +31,8 @@ $EM_CONF[$_EXTKEY] = Array (
 	'author_company' => 'd.k.d Internet Service GmbH, www.dkd.de',
 	'private' => '',
 	'download_password' => '',
-	'version' => '0.7.0',	// Don't modify this! Managed automatically during upload to repository.
-	'_md5_values_when_last_written' => 'a:8:{s:12:"ext_icon.gif";s:4:"2353";s:15:"ext_php_api.dat";s:4:"ddd5";s:14:"ext_tables.php";s:4:"ee65";s:12:"cli/conf.php";s:4:"8713";s:19:"cli/exec_bg_cli.php";s:4:"2dd5";s:19:"doc/wizard_form.dat";s:4:"1549";s:20:"doc/wizard_form.html";s:4:"e5ed";s:39:"sv1/class.tx_dkdstaticuploadscp_sv1.php";s:4:"a235";}',
+	'version' => '1.0.0',	// Don't modify this! Managed automatically during upload to repository.
+	'_md5_values_when_last_written' => 'a:7:{s:12:"ext_icon.gif";s:4:"2353";s:15:"ext_php_api.dat";s:4:"ddd5";s:14:"ext_tables.php";s:4:"ee65";s:19:"doc/wizard_form.dat";s:4:"1549";s:20:"doc/wizard_form.html";s:4:"e5ed";s:39:"sv1/class.tx_dkdstaticuploadscp_sv1.php";s:4:"4f11";s:15:"sv1/exec_bg.php";s:4:"a2a5";}',
 );
 
 ?>
